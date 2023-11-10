51a0a3820f03a37d11006f6f8ec7b7fa059bfd47
Jason L Perry <298+ambethia@users.noreply.github.com>
2023-11-09 21:23:23 -0500

    Setup Starship

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-DOTFILES_PATH="${DOTFILES_PATH:="$HOME/.dotfiles"}"
+DOTFILES="${DOTFILES:="$HOME/.dotfiles"}"
 
 if [[ "$CODESPACES" = "true" ]] && [[ -d /workspaces/.codespaces/.persistedshare/dotfiles ]]; then
-  ln -sf /workspaces/.codespaces/.persistedshare/dotfiles "$DOTFILES_PATH"
+  ln -sf /workspaces/.codespaces/.persistedshare/dotfiles "$DOTFILES"
 fi
 
 curl -sS https://starship.rs/install.sh | sh
+
+if [[ ! -d ~/.config ]]; then
+  mkdir -p ~/.config
+fi
+
+ln -sf "$DOTFILES/.zprofile" ~/.zprofile
+ln -sf "$DOTFILES/starship.toml" ~/.config/starship.toml
